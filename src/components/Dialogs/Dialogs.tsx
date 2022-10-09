fc75d71aabb0bb6428a45bb2866ff63cdacfee9c
Julia Titkova <juli.ogogo@yandex.ru>
2022-10-09 14:26:21 +0300

    23 - props for Dialogs (MessageItem)

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -16,37 +16,29 @@ const DialogItem = (props: DialogItemPropsType) => {
     );
 }
 
+type MessageItemPropsType = {
+    message: string
+}
+
+const MessageItem = (props: MessageItemPropsType) => {
+    return <div className={s.messageItem}>{props.message}</div>
+}
+
 const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-
                 <DialogItem name={"Julia"} id={1}/>
                 <DialogItem name={"Maxim"} id={2}/>
                 <DialogItem name={"Katya"} id={3}/>
                 <DialogItem name={"Alex"} id={4}/>
                 <DialogItem name={"Galya"} id={5}/>
-
-                {/*<div className={s.dialogItem}>
-                    <NavLink to="/dialogs/1">Julia</NavLink>
-                </div>
-                <div className={s.dialogItem}>
-                    <NavLink to="/dialogs/2">Maxim</NavLink>
-                </div>
-                <div className={s.dialogItem}>
-                    <NavLink to="/dialogs/3">Katya</NavLink>
-                </div>
-                <div className={s.dialogItem + ' ' + s.activeItem}>
-                    <NavLink to="/dialogs/4">Alex</NavLink>
-                </div>
-                <div className={s.dialogItem}>
-                    <NavLink to="/dialogs/5">Galya</NavLink>
-                </div>*/}
             </div>
+
             <div className={s.messagesItems}>
-                <div className={s.messageItem}>Hello!</div>
-                <div className={s.messageItem}>How are you?</div>
-                <div className={s.messageItem}>Lol :D</div>
+                <MessageItem message={'Hello!'}/>
+                <MessageItem message={'How are you?'}/>
+                <MessageItem message={'Lol :D'}/>
             </div>
         </div>
     );
