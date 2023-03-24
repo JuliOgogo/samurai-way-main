4c48f54ff7355c4c4e0fee0cb03d50290158b7ee
Julia Titkova <juli.ogogo@yandex.ru>
2023-03-24 21:04:04 +0300

    56 - Презентационная и Контейнерная компоненты

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import {DialogsContainer} from "./components/Dialogs/DialogsContainer";
 import {FriendsContainer} from "./components/Friends/FriendsContainer";
-import {UsersContainer} from "./components/Users/UsersContainer";
+import UsersContainer from "./components/Users/UsersContainer";
 
 export type AppPropsType = {}
 
--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -3,9 +3,51 @@ import {connect} from "react-redux";
 import {StateType, UserType} from "../../redux/types";
 import {followAC, setCurrentPageAC, setTotalCountAC, setUsersAC, unFollowAC} from "../../redux/usersReducer";
 import {AppDispatchType} from "../../redux/redux-store";
-import {UsersApiComponent} from "./UsersApiComponent";
+import axios from "axios";
+import {Users} from "./Users";
 
-export type UsersContainerPropsType = {}
+export type UsersContainerPropsType = {
+    users: UserType[]
+    totalUsersCount: number
+    pageSize: number
+    currentPage: number
+    follow: (userId: string) => void
+    unFollow: (userId: string) => void
+    setUsers: (users: UserType[]) => void
+    setCurrentPage: (currentPage: number) => void
+    setTotalCount: (setTotalCount: number) => void
+}
+
+export class UsersContainer extends React.Component<UsersContainerPropsType, any> {
+    /*constructor(props: UsersPropsType) {
+        super(props);
+    }*/
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(res => {
+            this.props.setUsers(res.data.items)
+            this.props.setTotalCount(res.data.totalCount)
+        })
+    }
+
+    changeCurrentPage = (currentPage: number) => {
+        this.props.setCurrentPage(currentPage)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${this.props.pageSize}`).then(res => {
+            this.props.setUsers(res.data.items)
+        })
+    }
+
+    render() {
+        return <Users users={this.props.users}
+                      follow={this.props.follow}
+                      unFollow={this.props.unFollow}
+                      totalUsersCount={this.props.totalUsersCount}
+                      pageSize={this.props.pageSize}
+                      currentPage={this.props.currentPage}
+                      changeCurrentPage={this.changeCurrentPage}
+        />
+    }
+}
 
 const mapStateToProps = (state: StateType) => {
     return {
@@ -25,4 +67,4 @@ const mapDispatchToProps = (dispatch: AppDispatchType) => {
     }
 }
 
-export const UsersContainer: React.FC<UsersContainerPropsType> = connect(mapStateToProps, mapDispatchToProps)(UsersApiComponent)
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer)
