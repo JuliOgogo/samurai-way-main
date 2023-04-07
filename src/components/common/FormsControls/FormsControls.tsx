78ba1c83b0e1b1eba39abff92cd7c901cc117da6
Julia Titkova <juli.ogogo@yandex.ru>
2023-04-08 00:03:53 +0300

    77 - redux-form field validation (валидация, ошибки). Осторожно Замыкание!

--- src/components/common/FormsControls/FormsControls.tsx
+++ src/components/common/FormsControls/FormsControls.tsx
@@ -1,32 +1,30 @@
 import React from "react";
 import s from './FormsControls.module.css'
 
-type TextareaPropsType = {
-    input: any
-    meta: any
+type FormControlPropsType = {
+    input?: any
+    meta?: any
 }
 
-export const Textarea: React.FC<TextareaPropsType> = ({input, meta, ...props}) => {
-
+const FormControl: React.FC<FormControlPropsType> = ({meta, ...props}) => {
     const error = meta.touched && meta.error
 
     return <div className={s.formsControls}>
-        <textarea {...input} {...props} className={error ? s.errorTextarea : ''}/><br/>
+        <div className={error ? s.errorTextarea : ''}>
+            {props.children}
+        </div>
         {error && <span className={s.errorSpan}>{meta.error}</span>}
     </div>
 }
 
-type InputPropsType = {
-    input: any
-    meta: any
+export const Textarea: React.FC<FormControlPropsType> = ({input, ...props}) => {
+    return <FormControl {...props}>
+        <textarea {...input} {...props}/>
+    </FormControl>
 }
 
-export const Input: React.FC<InputPropsType> = ({input, meta, ...props}) => {
-
-    const error = meta.touched && meta.error
-
-    return <div className={s.formsControls}>
-        <input {...input} {...props} className={error ? s.errorTextarea : ''}/><br/>
-        {error && <span className={s.errorSpan}>{meta.error}</span>}
-    </div>
+export const Input: React.FC<FormControlPropsType> = ({input, ...props}) => {
+    return <FormControl {...props} >
+        <input {...input} {...props}/>
+    </FormControl>
 }
